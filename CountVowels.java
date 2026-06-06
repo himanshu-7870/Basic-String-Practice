b4fb28e59b82a3ca049c2c881bef5d799b1941cd
Himanshu <hkp7439@gmail.com>
2026-06-07 02:35:34 +0530

    Basic String Practice

---
+++ CountVowels.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class CountVowels {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please enter a string");
+
+        String s = sc.nextLine();
+
+        int count = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch == 'a' || ch == 'i' || ch == 'e' || ch == 'o' || ch== 'u')
+            count++;
+        }
+
+        System.out.print(count);
+    }
+    
+}
---
+++ StringBasic.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class StringBasic {
+    public static void main(String[] args) {
+        
+    Scanner sc = new Scanner(System.in);
+
+    String s = "I am Himanshu Patel";
+    System.out.print(s);
+
+    //input
+    System.out.println("Please enter your name");
+    String st = sc.nextLine();
+
+    //output
+    System.out.print("Your name is"+st);
+    
+
+    }
+}
